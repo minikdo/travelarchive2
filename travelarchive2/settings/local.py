ef34f9fe904dbe4fe7e8c3b4d467def4c2deed39
Dominik Szmek <do@minik.pl>
2019-01-02 10:47:01 +0100

    add local settings

--- travelarchive2/settings/local.py
+++ travelarchive2/settings/local.py
@@ -1,1 +1,21 @@
-from .base import *
+from .base import * # noqa
+
+DEBUG = True
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
+
+INSTALLED_APPS += ['debug_toolbar', 'django_extensions']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+
+INTERNAL_IPS = ['127.0.0.1', '192.168.8.101']
+
+ALLOWED_HOSTS += ['192.168.8.101']
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # add STATIC_ROOT to DIRS
+]
