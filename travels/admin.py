7951fc1554838cd93e6bf1dc70ed4b7faae2aa7f
Dominik Szmek <do@minik.pl>
2019-01-02 10:47:47 +0100

    add travels models to admin

--- travels/admin.py
+++ travels/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Travel, Place, Country, Journey, Transport,\
+    Flight, Airport
 
 # Register your models here.
+admin.site.register(Travel)
+admin.site.register(Place)
+admin.site.register(Country)
+admin.site.register(Journey)
+admin.site.register(Transport)
+admin.site.register(Flight)
+admin.site.register(Airport)
