43278b7ce2244956269a5c833b1f93493f565675
Dominik Szmek <do@minik.pl>
2019-01-14 19:22:57 +0100

    add new models

--- travels/admin.py
+++ travels/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Travel, Place, Country, Journey, Transport,\
-    Flight, Airport
+    Flight, Airport, Currency
 
 # Register your models here.
 admin.site.register(Travel)
@@ -10,3 +10,4 @@ admin.site.register(Journey)
 admin.site.register(Transport)
 admin.site.register(Flight)
 admin.site.register(Airport)
+admin.site.register(Currency)
